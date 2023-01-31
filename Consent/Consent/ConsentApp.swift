06d9f7339bf118053986157d3c051cb65d21473f
Lusine <lusine@getsteven.com>
2023-02-01 00:43:15 +0400

    add xcode project

---
+++ Consent/Consent/ConsentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsentApp.swift
+//  Consent
+//
+//  Created by Lusine Magauzyan on 01.02.23.
+//
+
+import SwiftUI
+
+@main
+struct ConsentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
